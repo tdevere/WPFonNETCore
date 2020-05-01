c7c29b4fea9755162503416ebc059725eff483b5
tdevere <anthony_devere@hotmail.com>
2020-05-01 16:55:44 -0500

    fixed default namespace :)

--- WPFonNETCore/MainWindow.xaml.cs
+++ WPFonNETCore/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WFPonNETCore
+namespace WPFonNETCore
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
